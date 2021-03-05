0daa0fa48e1f118116ab70d0230aeeb60d2abb72
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-05 17:08:53 +1100

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply =  num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()+1);
+
+const num4 = 18;
+const num3 = 7; 
+const mod = num4 % num3;
+
+const max = Math.max(16, 20);
